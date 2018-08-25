c95210bc7d53d86d1d2ae5540ee5d1234dfa54fa
Alexandre Canoën <contact@acanoen.fr>
2018-08-25 10:59:08 +0200

    initial commit

--- templates/layouts/default.php
+++ templates/layouts/default.php
@@ -4,10 +4,10 @@
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
             <title><?= $title ?></title>
-            <link rel="stylesheet" type="text/css" href="<?= $_ENV["dirs"]["public"] ?>/css/app.css" />
+            <link rel="stylesheet" type="text/css" href="css/app.css" />
         </head>
         <body>
             <?= $content_for_layout ?>
-            <script type="text/javascript" src="<?= $_ENV["dirs"]["public"] ?>/js/app.js"></script>
+            <script type="text/javascript" src="js/app.js"></script>
         </body>
     </html>
