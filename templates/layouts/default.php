c128285fa1bb7f52ec0cd54d285ac2a6d81b326e
Alexandre Canoën <contact@acanoen.fr>
2018-08-29 16:57:58 +0200

    initial commit

--- templates/layouts/default.php
+++ templates/layouts/default.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-    <html>
+    <html lang="fr">
         <head>
             <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <title><?= $title ?></title>
+            <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
             <link rel="stylesheet" type="text/css" href="css/app.css" />
         </head>
         <body>
             <?= $content_for_layout ?>
+            <script type="text/javascript" src="js/jquery.min.js"></script>
+            <script type="text/javascript" src="js/popper.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.min.js"></script>
             <script type="text/javascript" src="js/app.js"></script>
         </body>
     </html>
